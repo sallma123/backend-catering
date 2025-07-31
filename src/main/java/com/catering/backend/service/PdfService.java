d4e1a107e02ba23aca137ae73a97dcd98d35e23d
salma <salmasedrati18@gmail.com>
2025-07-31 17:22:28 +0100

    modifier la fiche

--- src/main/java/com/catering/backend/service/PdfService.java
+++ src/main/java/com/catering/backend/service/PdfService.java
@@ -20,6 +20,16 @@ public class PdfService {
     @Autowired
     private CommandeRepository commandeRepository;
 
+    private Font getCalibriFont(float size, int style) {
+        try {
+            BaseFont baseFont = BaseFont.createFont("src/main/resources/fonts/calibri.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            return new Font(baseFont, size, style);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return FontFactory.getFont(FontFactory.HELVETICA, size, style);
+        }
+    }
+
     public byte[] genererFicheCommande(Long commandeId) throws Exception {
         Commande commande = commandeRepository.findById(commandeId)
                 .orElseThrow(() -> new Exception("Commande introuvable"));
@@ -29,13 +39,13 @@ public class PdfService {
                 .collect(Collectors.toList());
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Document document = new Document(PageSize.A4, 36, 36, 120, 100);
+        Document document = new Document(PageSize.A4, 41, 41, 70, 100);
         PdfWriter writer = PdfWriter.getInstance(document, out);
         writer.setPageEvent(new HeaderFooterEvent());
 
         document.open();
 
-// ➤ Ligne avec Fiche technique N°... à gauche et Rabat le : ... à droite
+        // ➤ Ligne avec Fiche technique N°... à gauche et Rabat le : ... à droite
         String dateFiche = (commande.getDateFiche() != null)
                 ? new SimpleDateFormat("dd/MM/yyyy").format(java.sql.Date.valueOf(commande.getDateFiche()))
                 : "??/??/????";
@@ -44,29 +54,27 @@ public class PdfService {
         headerLine.setWidthPercentage(100);
         headerLine.setWidths(new float[]{4f, 1f, 4f});
 
-        Font normal16 = FontFactory.getFont(FontFactory.HELVETICA, 16, Font.NORMAL);
+        Font calibri16 = getCalibriFont(16, Font.NORMAL);
 
-// Cellule à gauche : Fiche technique
-        PdfPCell leftCell = new PdfPCell(new Phrase("Fiche technique N° " + commande.getNumeroCommande(), normal16));
+        PdfPCell leftCell = new PdfPCell(new Phrase("Fiche technique N° " + commande.getNumeroCommande(), calibri16));
         leftCell.setHorizontalAlignment(Element.ALIGN_LEFT);
         leftCell.setBorder(Rectangle.NO_BORDER);
 
-// Cellule vide pour espacement
         PdfPCell spaceCell = new PdfPCell(new Phrase(""));
         spaceCell.setBorder(Rectangle.NO_BORDER);
 
-// Cellule à droite : Rabat le
-        PdfPCell rightCell = new PdfPCell(new Phrase("Rabat le : " + dateFiche, normal16));
+        PdfPCell rightCell = new PdfPCell(new Phrase("Rabat le : " + dateFiche, calibri16));
         rightCell.setHorizontalAlignment(Element.ALIGN_RIGHT);
         rightCell.setBorder(Rectangle.NO_BORDER);
 
         headerLine.addCell(leftCell);
         headerLine.addCell(spaceCell);
         headerLine.addCell(rightCell);
-
         document.add(headerLine);
-        document.add(new Paragraph(" "));
 
+        // ➤ Informations client
+        Font calibri11 = getCalibriFont(11, Font.NORMAL);
+        Font calibri11Bold = getCalibriFont(11, Font.BOLD);
 
         PdfPTable infoTable = new PdfPTable(2);
         infoTable.setWidthPercentage(100);
@@ -74,26 +82,27 @@ public class PdfService {
 
         PdfPCell left = new PdfPCell();
         left.setBorder(Rectangle.NO_BORDER);
-        left.addElement(new Paragraph("Client : " + commande.getNomClient()));
-        left.addElement(new Paragraph("Nbre de " + (commande.getTypeClient().name().equals("ENTREPRISE") ? "personnes" : "tables") + " : " + commande.getNombreTables()));
+        left.addElement(new Paragraph("Client : " + commande.getNomClient(), calibri11));
+        left.addElement(new Paragraph("Nbre de " + (commande.getTypeClient().name().equals("ENTREPRISE") ? "personnes" : "tables") + " : " + commande.getNombreTables(), calibri11));
 
         PdfPCell right = new PdfPCell();
         right.setBorder(Rectangle.NO_BORDER);
-        right.addElement(new Paragraph("Date : " + commande.getDate()));
-        right.addElement(new Paragraph("Salle : " + commande.getSalle()));
+        right.addElement(new Paragraph("Date : " + commande.getDate(), calibri11));
+        right.addElement(new Paragraph("Salle : " + commande.getSalle(), calibri11));
 
         infoTable.addCell(left);
         infoTable.addCell(right);
         document.add(infoTable);
         document.add(new Paragraph(" "));
 
+        // ➤ Tableau principal
         PdfPTable table = new PdfPTable(4);
         table.setWidthPercentage(100);
         table.setWidths(new float[]{4, 1, 2, 2});
 
         String[] headers = {"Désignation", "Quantité", "PU (DH)", "Total"};
         for (String header : headers) {
-            PdfPCell headerCell = new PdfPCell(new Phrase(header, FontFactory.getFont(FontFactory.HELVETICA_BOLD, 11)));
+            PdfPCell headerCell = new PdfPCell(new Phrase(header, calibri11Bold));
             table.addCell(headerCell);
         }
 
@@ -105,8 +114,8 @@ public class PdfService {
         for (String categorie : produitsParCategorie.keySet()) {
             List<ProduitCommande> produits = produitsParCategorie.get(categorie);
 
-            // Ligne section AVEC 4 cellules pour respecter la structure
-            PdfPCell sectionDesignation = new PdfPCell(new Phrase(categorie + " :", FontFactory.getFont(FontFactory.HELVETICA_BOLD, 12)));
+            // ➤ Ligne de titre de section
+            PdfPCell sectionDesignation = new PdfPCell(new Phrase(categorie + " :", getCalibriFont(12, Font.BOLD)));
             PdfPCell sectionQte = new PdfPCell(new Phrase(""));
             PdfPCell sectionPU = new PdfPCell(new Phrase(""));
             PdfPCell sectionTotal = new PdfPCell(new Phrase(""));
@@ -117,10 +126,10 @@ public class PdfService {
             table.addCell(sectionTotal);
 
             for (ProduitCommande produit : produits) {
-                PdfPCell cellDesignation = new PdfPCell(new Phrase(" ‐ " + produit.getNom()));
-                PdfPCell cellQte = new PdfPCell(new Phrase(""));
-                PdfPCell cellPU = new PdfPCell(new Phrase(""));
-                PdfPCell cellTotal = new PdfPCell(new Phrase(""));
+                PdfPCell cellDesignation = new PdfPCell(new Phrase(" ‐ " + produit.getNom(), calibri11));
+                PdfPCell cellQte = new PdfPCell(new Phrase("", calibri11));
+                PdfPCell cellPU = new PdfPCell(new Phrase("", calibri11));
+                PdfPCell cellTotal = new PdfPCell(new Phrase("", calibri11));
 
                 if (categorie.equalsIgnoreCase("Supplément") ||
                         List.of("Matériel et Service", "Prestataires", "Pièce montée").contains(categorie)) {
@@ -128,9 +137,9 @@ public class PdfService {
                     double prix = produit.getPrix();
                     double montant = quantite * prix;
 
-                    cellQte.setPhrase(new Phrase(String.valueOf(quantite)));
-                    cellPU.setPhrase(new Phrase(String.format("%.2f", prix)));
-                    cellTotal.setPhrase(new Phrase(String.format("%.2f", montant)));
+                    cellQte.setPhrase(new Phrase(String.valueOf(quantite), calibri11));
+                    cellPU.setPhrase(new Phrase(String.format("%.2f", prix), calibri11));
+                    cellTotal.setPhrase(new Phrase(String.format("%.2f", montant), calibri11));
 
                     totalSuppl += montant;
                 }
@@ -145,19 +154,14 @@ public class PdfService {
         document.add(table);
         document.add(new Paragraph(" "));
 
+        // ➤ Total
         double totalGeneral = commande.getPrixParTable() * commande.getNombreTables() + totalSuppl;
 
         Paragraph totalParag = new Paragraph("Total prestation : " + String.format("%.2f", totalGeneral) + " DH",
-                FontFactory.getFont(FontFactory.HELVETICA_BOLD, 13));
+                getCalibriFont(13, Font.BOLD));
         totalParag.setAlignment(Element.ALIGN_CENTER);
         document.add(totalParag);
         document.add(new Paragraph(" "));
-
-        Paragraph sign = new Paragraph("BELMOKHTAR karting SARL ‐ Services traiteur\n" +
-                "Site web : hs-traiteur.com, contact : contact@hs-traiteur.com, Tél : 0661966939",
-                FontFactory.getFont(FontFactory.HELVETICA, 9));
-        sign.setAlignment(Element.ALIGN_CENTER);
-        document.add(sign);
 
         document.close();
         writer.close();
